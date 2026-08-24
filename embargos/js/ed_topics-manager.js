285c11d062c743ec8fb86647f0f45e4586c0b1a5
fernnog-gab <fernandocnogueira1@gmail.com>
2026-08-23 22:29:40 -0400

    fix(ui-ed): corrige dimensionamento e icone do botão de leitura de pilhas

--- embargos/js/ed_topics-manager.js
+++ embargos/js/ed_topics-manager.js
@@ -267,7 +267,10 @@ window.TopicsManager = (function () {
                 </div>
                 
                 <div class="btn-read-mode-trigger sub-read-badge" title="Modo Leitura do Grupo" onclick="TopicsManager.abrirModoLeituraPilha('${activeTabId}', '${sub.grupoId}', '${numRomano}')">
-                    <svg><use href="#icon-book-open"></use></svg>
+                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                        <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+                    </svg>
                 </div>
             </div>
         </div>`;
